d5570f02fd6048e650acb42de3ed0d216ad2efdc
Luan <luanamorimds@gmail.com>
2025-12-10 10:21:55 -0400

    feat: update supabase url

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tcpksngkkbonvxjdobip.supabase.co';
+const supabaseUrl = 'https://cuovnpafzdinzdbwwiqj.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey || '');
